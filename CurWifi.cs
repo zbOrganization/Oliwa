b7339868bdce9720bce81b9546c0ac819e622e06
misha kevlishvili <kevlishvilimisha@yahoo.com>
2015-05-23 08:58:22 +0400

    Update CurWifi.cs
    test branch

--- CurWifi.cs
+++ CurWifi.cs
@@ -5,7 +5,7 @@ public class CurWifi
     private string sSID;
     private string bSSID;
     private int signal;
-    public CurWifi(string SSID, string BSSID,int signal)
+    public CurWifi(string SSID,string BSSID,int signal)
     {
         this.bSSID = BSSID;
         this.sSID = SSID;
